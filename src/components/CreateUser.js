0019665b1f12da0a96de0879f42361a288e0ad94
Milo Rastgoo <msrastgoo@gmail.com>
2020-07-22 18:31:53 -0400

    create user component

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const CreateUser = () => {
 
+    const [ user, setUser ] = useState({
+        username:'',
+    })
+
+    const onChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+        console.log(user)
+    }
+
     return (
+        <>
         <h1>Create New User</h1>
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label>Username:</label>
+                <input type="text"
+                    required
+                    className="form-control"
+                    name="username"
+                    value={user.username}
+                    onChange={onChange}
+                    />
+            </div>
+            <div className="form-group">
+                <input type="submit" value="Create User" className="btn btn-primary" />
+            </div>
+        </form>
+        </>
     )
 }
 
